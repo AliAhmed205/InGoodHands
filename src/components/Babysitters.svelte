26399c6d8cefa4f61d7dd6f02d13c3f64736ab82
AliAhmed205 <118130116+AliAhmed205@users.noreply.github.com>
2024-11-05 15:38:40 +0100

    Created babysitter.json and a babysitter component which will be a slideshow.

---
+++ src/components/Babysitters.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+<section>
+  <ul class="about-us">
+    <li><a href="#"></a></li>
+    <li><a href="#"></a></li>
+    <li><a href="#"></a></li>
+    <li><a href="#"></a></li>
+    <li><a href="#"></a></li>
+    <li><a href="#"></a></li>
+    <li><a href="#"></a></li>
+    <li><a href="#"></a></li>
+    <li><a href="#"></a></li>
+    <li><a href="#"></a></li>
+  </ul>
+</section>
