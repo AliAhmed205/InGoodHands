ba277c945649efc17f661dbf8a8dd06cc8f8ef7b
AliAhmed205 <118130116+AliAhmed205@users.noreply.github.com>
2024-11-06 15:10:13 +0100

    Added a try, catch and finally due to sudden stop from fetching the data. Added an active class to detail-wrapper.

--- src/routes/babysitters/[id]/+page.svelte
+++ src/routes/babysitters/[id]/+page.svelte
@@ -1,41 +1,58 @@
 <script>
   import { page } from "$app/stores";
-  import { onMount } from "svelte";
+  import { onMount, onDestroy } from "svelte";
 
   let babysitter = null;
   let loading = true;
   const { id } = $page.params;
 
-  onMount(() => {
-    fetch(
-      "https://raw.githubusercontent.com/AliAhmed205/InGoodHands/refs/heads/main/src/Database/about-babysitters.json"
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(
-            "Netwerk response was niet ok: " + response.statusText
-          );
-        }
-        return response.json();
-      })
-      .then((babysitters) => {
-        babysitter = babysitters.find((b) => String(b.id) === String(id));
+  onMount(async () => {
+    try {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/AliAhmed205/InGoodHands/refs/heads/main/src/Database/about-babysitters.json"
+      );
 
-        if (!babysitter) {
-          throw new Error("Babysitter niet gevonden");
-        }
-
-        loading = false;
-      })
-      .catch((error) => {
-        console.error(
-          "Er was een probleem met het ophalen van de babysitter data:",
-          error
+      if (!response.ok) {
+        throw new Error(
+          "Netwerk response was niet ok: " + response.statusText
         );
-        loading = false;
-      });
+      }
+
+      const babysitters = await response.json();
+      babysitter = babysitters.find((b) => String(b.id) === String(id));
+
+      if (!babysitter) {
+        throw new Error("Babysitter niet gevonden");
+      }
+    } catch (error) {
+      console.error(
+        "Er was een probleem met het ophalen van de babysitter data:",
+        error
+      );
+    } finally {
+      loading = false;  
+    }
+
+    const handleScroll = () => {
+      const cardDetail = document.getElementById("detail-wrapper");
+      if (cardDetail) {
+        if (window.scrollY > 10) {
+          cardDetail.classList.add("active");
+        } else {
+          cardDetail.classList.remove("active");
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    onDestroy(() => {
+      window.removeEventListener("scroll", handleScroll);
+    });
   });
 </script>
+
+
 
 {#if loading}
   <p>Loading...</p>
@@ -43,33 +60,33 @@
   <section class="babysitter-detail-container">
     <section class="image-container">
       <div class="text-container">
-      <h1>
-        {babysitter.naam}
-        <i class="fa-solid fa-certificate"></i>
-      </h1>
-      <h3>€ {babysitter.uurtarief},00/hour</h3>
-    </div>
+        <h1>
+          {babysitter.naam}
+          <i class="fa-solid fa-certificate"></i>
+        </h1>
+        <h3>€ {babysitter.uurtarief},00/hour</h3>
+      </div>
       <img src={babysitter.profielFoto} alt={`Foto van ${babysitter.naam}`} />
     </section>
 
-    <section class="detail-wrapper">
+    <section class="detail-wrapper" id="detail-wrapper">
       <section class="detail one">
         <p><i class="fa-solid fa-thumbtack"></i> {babysitter.locatie}</p>
         <div class="star-container">
           {#each Array(babysitter.gemiddeldeReviewScore) as _, i}
             <i class="fa-solid fa-star" key={i}></i>
           {/each}
-          <p>({babysitter.gemiddeldeReviewScore}) Reviews</p>
+          <p>({babysitter.totaalAantalReviews}) Reviews</p>
         </div>
       </section>
-      <hr>
+      <hr />
       <p class="babysitter-bio">{babysitter.bio}</p>
-      <hr>
+      <hr />
       <div class="experience">
         <i class="fa-regular fa-address-card"></i>
         <div class="experience-text">
           <h3>Babysit Experience</h3>
-            <p>{babysitter.ervaring} years</p>
+          <p>{babysitter.ervaring} years</p>
         </div>
       </div>
       <div class="experience">
@@ -78,50 +95,76 @@
           <h3>Has experience with</h3>
           <ul>
             {#each babysitter.ervaringKind as kind}
-            <li>{kind}</li>
-          {/each}
+              <li>{kind}</li>
+            {/each}
           </ul>
         </div>
       </div>
-      <hr>
-      <a href="/babysitters">Terug naar de lijst</a>
+      <hr />
       <div class="babysitter-schedule">
+        <table>
+          <caption> Beschikbaarheid </caption>
+          <thead>
+            <tr>
+              <th scope="col"></th>
+              {#each Object.keys(babysitter.beschikbaarheid) as dag}
+                <th scope="col">{dag}</th>
+              {/each}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Ochtend</th>
+              {#each Object.keys(babysitter.beschikbaarheid) as dag}
+                <td>
+                  {#if babysitter.beschikbaarheid[dag].ochtend}
+                    <i class="fa-solid fa-circle"></i>
+                  {:else}
+                    <i class="fa-regular fa-circle"></i>
+                  {/if}
+                </td>
+              {/each}
+            </tr>
+            <tr>
+              <th scope="row">Middag</th>
+              {#each Object.keys(babysitter.beschikbaarheid) as dag}
+                <td>
+                  {#if babysitter.beschikbaarheid[dag].middag}
+                    <i class="fa-solid fa-circle"></i>
+                  {:else}
+                    <i class="fa-regular fa-circle"></i>
+                  {/if}
+                </td>
+              {/each}
+            </tr>
+            <tr>
+              <th scope="row">Namiddag</th>
+              {#each Object.keys(babysitter.beschikbaarheid) as dag}
+                <td>
+                  {#if babysitter.beschikbaarheid[dag].namiddag}
+                    <i class="fa-solid fa-circle"></i>
+                  {:else}
+                    <i class="fa-regular fa-circle"></i>
+                  {/if}
+                </td>
+              {/each}
+            </tr>
+            <tr>
+              <th scope="row">Avond</th>
+              {#each Object.keys(babysitter.beschikbaarheid) as dag}
+                <td>
+                  {#if babysitter.beschikbaarheid[dag].avond}
+                    <i class="fa-solid fa-circle"></i>
+                  {:else}
+                    <i class="fa-regular fa-circle"></i>
+                  {/if}
+                </td>
+              {/each}
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <table>
-        <caption>
-          beschikbaar
-        </caption>
-        <thead>
-          <tr>
-            <th scope="col">Person</th>
-            <th scope="col">Most interest in</th>
-            <th scope="col">Age</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">Chris</th>
-            <td>HTML tables</td>
-            <td>22</td>
-          </tr>
-          <tr>
-            <th scope="row">Dennis</th>
-            <td>Web accessibility</td>
-            <td>45</td>
-          </tr>
-          <tr>
-            <th scope="row">Sarah</th>
-            <td>JavaScript frameworks</td>
-            <td>29</td>
-          </tr>
-          <tr>
-            <th scope="row">Karen</th>
-            <td>Web performance</td>
-            <td>36</td>
-          </tr>
-        </tbody>
-      </table>
-      
+      <a href="/babysitters">Terug naar de lijst</a>
     </section>
   </section>
 {:else}
