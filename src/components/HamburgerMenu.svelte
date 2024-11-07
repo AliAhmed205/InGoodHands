3ad2c68387ea95b15037f88b134a1c34d9089de1
AliAhmed205 <118130116+AliAhmed205@users.noreply.github.com>
2024-11-07 15:46:28 +0100

    Hamburger component made and a-tag removed from the about-us.

---
+++ src/components/HamburgerMenu.svelte
@@ -0,0 +1,29 @@
+<script>
+  import "../components/CSS/Header.css"
+</script>
+
+<ul class="mobile">
+  <div class="top-menu">
+    <h1><i class="fa-solid fa-person-breastfeeding"></i>In Good Hands</h1>
+    <button type="button" id="close-menu"
+      ><i class="fa-solid fa-xmark"></i></button
+    >
+  </div>
+  <hr />
+  <li>
+    <a href="#about-us-link">about</a>
+  </li>
+  <hr />
+  <li>
+    <a href="#review-anchor">reviews</a>
+  </li>
+  <hr />
+  <li>
+    <a href="#FAQ-id">FAQ</a>
+  </li>
+  <hr />
+  <li>
+    <a href="/babysitters" class="CTA">Book a babysitter</a>
+  </li>
+  <hr />
+</ul>
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from "svelte";
   import "../components/CSS/Header.css"
+  import HamburgerMenu from "./HamburgerMenu.svelte";
 
   onMount(() => {
     const hamburgerIcon = document.getElementById("hamburger-icon");
@@ -59,31 +60,6 @@
     <button type="button" id="hamburger-icon"
       ><i class="fa-solid fa-bars"></i></button
     >
-
-    <ul class="mobile">
-      <div class="top-menu">
-        <h1><i class="fa-solid fa-person-breastfeeding"></i>In Good Hands</h1>
-        <button type="button" id="close-menu"
-          ><i class="fa-solid fa-xmark"></i></button
-        >
-      </div>
-      <hr />
-      <li>
-        <a href="#about-us-link">about</a>
-      </li>
-      <hr />
-      <li>
-        <a href="#review-anchor">reviews</a>
-      </li>
-      <hr />
-      <li>
-        <a href="#FAQ-id">FAQ</a>
-      </li>
-      <hr />
-      <li>
-        <a href="/babysitters" class="CTA">Book a babysitter</a>
-      </li>
-      <hr />
-    </ul>
+    <HamburgerMenu />
   </nav>
 </header>
